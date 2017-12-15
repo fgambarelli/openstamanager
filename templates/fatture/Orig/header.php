3c0fa0da1af119250660c61a32fe2fb68abdadea
Chica <chica@MacBook-Pro-di-Francesca.local>
2017-12-15 14:04:26 +0100

    STAMPE

--- templates/fatture/Orig/header.php
+++ templates/fatture/Orig/header.php
@@ -2,25 +2,13 @@
 
 echo '
 <!-- Intestazione fornitore -->
-<div class="row">
-    <div class="col-xs-6">
-        <img src="'.__DIR__.'/logo_azienda.jpg" alt="Logo" border="0"/>
-    </div>
-    <div class="col-xs-6 text-right">
-        <p><b>$f_ragionesociale$</b></p>
-        <p>'.tr('Sede Legale e Amm:').'.$f_indirizzo$ $f_citta_full$</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr('C.F.').': ' : '').'$f_codicefiscale$</p>
-        <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': ' : '').'$f_capsoc$</p>
-        <p>'.(!empty($f_telefono) ? tr('Tel').': ' : '').'$f_telefono$</p>
-    </div>
-</div>
-
+$default_header$
 <br>
 
 <div class="row">
     <!-- Dati Fattura -->
     <div class="col-xs-6">
-        <div class="text-left" style="height:5mm;">
+        <div class="text-center" style="height:5mm;">
             <b>$tipo_doc$</b>
         </div>
 
@@ -28,37 +16,25 @@ echo '
             <tr>
                 <td valign="top" class="border-full text-center">
                     <p class="small-bold">'.tr('Nr. documento', [], ['upper' => true]).'</p>
-                    
+                    <p>$numero_doc$</p>
                 </td>
 
                 <td class="border-right border-bottom border-top text-center">
                     <p class="small-bold">'.tr('Data documento', [], ['upper' => true]).'</p>
-                    
-                </td>
-
-
-                <td class="border-right border-bottom border-top center text-center">
-                    <p class="small-bold">'.tr('Foglio', [], ['upper' => true]).'</p>
-                    
-                </td>
-            </tr>
-            <tr>
-                <td valign="top" class="border-full text-center">
-                    
-                    <p><b>$numero_doc$</b></p>
+                    <p>$data$</p>
                 </td>
 
                 <td class="border-right border-bottom border-top text-center">
-                    
-                    <p><b>$data$</b></p>
+                    <p class="small-bold">'.tr('Cliente', [], ['upper' => true]).'</p>
+                    <p>$c_codice$</p>
                 </td>
 
-
                 <td class="border-right border-bottom border-top center text-center">
-                    
+                    <p class="small-bold">'.tr('Foglio', [], ['upper' => true]).'</p>
                     <p>{PAGENO}/{nb}</p>
                 </td>
             </tr>
+
             <tr>
                 <td colspan="2" style="height:10mm;padding-top:2mm;">
                     <p class="small-bold">'.tr('Pagamento', [], ['upper' => true]).'</p>
