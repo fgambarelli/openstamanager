486c1f6e84f0d64e1d99d5d3438959a09a693f6e
Chica <chica@MacBook-Pro-di-Francesca.local>
2017-12-27 15:58:10 +0100

    INTERVENTI - Tipologia Impianto

--- templates/interventi/body.php
+++ templates/interventi/body.php
@@ -17,7 +17,7 @@ echo '
 
     <tr>
         <td class="text-center" style="width:20%">'.tr('Num. Interno').': <b>'.$records[0]['codice'].'</b></td>
-        <td class="text-center" style="width:20%">'.tr('Rapportino num.').': <b>'.$records[0]['codice'].'</b></td>
+        <td class="text-center" style="width:20%">'.tr('Rapportino num.').': <b>'.$records[0]['num_rapp'].'</b></td>
         <td class="text-center" style="width:20%">'.tr('Data').': <b>'.Translator::dateToLocale($records[0]['data_richiesta']).'</b></td>
         <td class="text-center" style="width:20%">'.tr('Preventivo num.').': <b>'.$records[0]['numero_preventivo'].'</b></td>
         <td class="text-center" style="width:20%">'.tr('Contratto num.').': <b>'.$records[0]['numero_contratto'].'</b></td>
