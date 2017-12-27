63c0cb47ca342c10c87593d21cbc1f21ab5d0d53
Chica <chica@MacBook-Pro-di-Francesca.local>
2017-12-28 00:13:49 +0100

    SQL

--- SQL/operazioni.sql
+++ SQL/operazioni.sql
@@ -3,3 +3,5 @@
 --Pulizia DB
 
 ALTER TABLE `in_interventi` ADD `num_rapp` VARCHAR(50) NULL AFTER `tipo_sconto_globale`;
+
+ALTER TABLE `my_impianti` ADD `idtipoimpianto` int(11) NULL AFTER `interno`;
