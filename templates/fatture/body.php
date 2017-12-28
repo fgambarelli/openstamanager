b4b1d03b0ce9c0f0ab672bc2c87438283d41f53f
Chica <chica@MacBook-Pro-di-Francesca.local>
2017-12-28 17:21:13 +0100

    FATTURE - Impianti

--- templates/fatture/body.php
+++ templates/fatture/body.php
@@ -173,10 +173,10 @@ echo '
 
 
 // AGGIUNGO TABELLA IMPIANTI
-$rs2 = $dbo->fetchArray('SELECT DISTINCT my_impianti_tipiimpianto.descrizione AS tipo_impianto, my_impianti.nome AS nome, my_impianti.matricola AS matricola FROM co_righe_documenti JOIN my_impianti_interventi ON co_righe_documenti.idintervento=my_impianti_interventi.idintervento JOIN my_impianti ON my_impianti.id = my_impianti_interventi.idimpianto JOIN my_impianti_tipiimpianto ON my_impianti_tipiimpianto.id=my_impianti.idtipoimpianto WHERE co_righe_documenti.iddocumento='.prepare($iddocumento));
+$rs2 = $dbo->fetchArray('SELECT DISTINCT my_impianti_tipiimpianto.descrizione AS tipo_impianto, my_impianti.nome AS nome, my_impianti.descrizione AS descrizione, my_impianti.matricola AS matricola FROM co_righe_documenti JOIN my_impianti_interventi ON co_righe_documenti.idintervento=my_impianti_interventi.idintervento JOIN my_impianti ON my_impianti.id = my_impianti_interventi.idimpianto JOIN my_impianti_tipiimpianto ON my_impianti_tipiimpianto.id=my_impianti.idtipoimpianto WHERE co_righe_documenti.iddocumento='.prepare($iddocumento));
 $impianti = [];
 for ($j = 0; $j < sizeof($rs2); ++$j) {
-    $impianti[] = '<b> ['.$rs2[$j]['tipo_impianto'].'] - '.$rs2[$j]['nome']."</b> <small style='color:#777;'>(".$rs2[$j]['matricola'].')</small>';
+    $impianti[] = '<b> ['.$rs2[$j]['tipo_impianto'].'] - '.$rs2[$j]['nome'].'-'.$rs2[$j]['descrizione']."</b> <small style='color:#777;'>(".$rs2[$j]['matricola'].')</small>';
 }
 if (!empty($rs2[0]['nome'])) {
       echo '
