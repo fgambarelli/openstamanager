40196726f0215b3b09bcb2c78ec8f4d23a29f613
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-01-05 19:27:40 +0100

    CONTRATTI - aggiunta campi

--- SQL/operazioni.sql
+++ SQL/operazioni.sql
@@ -5,3 +5,6 @@
 ALTER TABLE `in_interventi` ADD `num_rapp` VARCHAR(50) NULL AFTER `tipo_sconto_globale`;
 
 ALTER TABLE `my_impianti` ADD `idtipoimpianto` int(11) NULL AFTER `interno`;
+
+ALTER TABLE `co_contratti` ADD `visite_num` INT(10) NOT NULL AFTER `tipo_sconto_globale`;
+ALTER TABLE `co_contratti` ADD `reperibilita` TINYINT(1)  NOT NULL AFTER `tipo_sconto_globale`;
