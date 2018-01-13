04e8b252ad299ed131aacf2a6d3ab474ec3c8539
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-01-13 11:31:02 +0100

    ARTICOLI - Aggiunto visualizzazione cliente in movimenti

--- SQL/operazioni.sql
+++ SQL/operazioni.sql
@@ -10,3 +10,11 @@ ALTER TABLE `co_contratti` ADD `visite_num` INT(10) NOT NULL AFTER `tipo_sconto_
 ALTER TABLE `co_contratti` ADD `reperibilita` TINYINT(1)  NOT NULL AFTER `tipo_sconto_globale`;
 ALTER TABLE `co_contratti` ADD `controllo_num` INT(10) NOT NULL AFTER `visite_num`;
 ALTER TABLE `co_contratti` ADD `tempo_medio` INT(10) NOT NULL AFTER `controllo_num`;
+
+CREATE VIEW `v_mg_movimenti_all` AS 
+select  mg_movimenti.*,
+(select `an_anagrafiche`.`ragione_sociale` from `an_anagrafiche` where (`an_anagrafiche`.`idanagrafica` =
+		(case when (`mg_movimenti`.`iddocumento` > 0) then (select `co_documenti`.`idanagrafica` from `co_documenti` where (`co_documenti`.`id` = `mg_movimenti`.`iddocumento`))
+			  else (case when (`mg_movimenti`.`idintervento` > 0) then (select `in_interventi`.`idanagrafica` from `in_interventi` where (`in_interventi`.`id` = `mg_movimenti`.`idintervento`)) end) end))
+) AS `cliente`
+from `mg_movimenti`;
