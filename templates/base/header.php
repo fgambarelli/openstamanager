ad37bc1cb3e195e74d6f6aa0bbe722f5c488f5c0
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-01-18 17:29:12 +0100

    STAMPE - Intestazione

--- templates/base/header.php
+++ templates/base/header.php
@@ -15,10 +15,11 @@ return '
         <img src="'.DOCROOT.'/templates/'.$ptype.'/logo_azienda.jpg" alt="Logo" border="0"/>
     </div>
     <div class="col-xs-6 text-right">
-        <p><b>$f_ragionesociale$</b></p>
-        <p>'.tr('Sede : ').'.$f_indirizzo$ $f_citta_full$</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr('C.F.').': ' : '').'$f_codicefiscale$</p>
-        <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': ' : '').'$f_capsoc$</p>
-        <p>'.(!empty($f_telefono) ? tr('Tel').': ' : '').'$f_telefono$</p>
+        <p><b>'.$f_ragionesociale.'</b></p>
+        <p>'.$f_indirizzo.' '.$f_citta_full.'</p>
+        <p>'.(!empty($f_piva) ? tr('P.Iva').': '.$f_piva : '').'</p>
+        <p>'.(!empty($f_codicefiscale) ? tr('C.F.').': '.$f_codicefiscale : '').'</p>
+        <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': '.$f_capsoc : '').'</p>
+        <p>'.(!empty($f_telefono) ? tr('Tel').': '.$f_telefono : '').'</p>
     </div>
 </div>';
