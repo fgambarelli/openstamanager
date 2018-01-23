f5c8a0ea455b1df4f49c7f56264db0b4bdb1b109
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-01-23 23:00:13 +0100

    CHIAMATA - Abbonato

--- templates/chiamata/body.php
+++ templates/chiamata/body.php
@@ -19,7 +19,7 @@ echo '
         <td class="text-center" style="width:20%">'.tr('Num. Interno').': <b>'.$records[0]['codice'].'</b></td>
         <td class="text-center" style="width:20%">'.tr('Data').': <b>'.Translator::dateToLocale($records[0]['data_richiesta']).'</b></td>
         <td class="text-center" style="width:20%">'.tr('Preventivo num.').': <b>'.$records[0]['numero_preventivo'].'</b></td>
-        <td class="text-center" style="width:20%">'
+        <td class="text-center" style="width:20%">';
         if (!empty($records[0]['numero_contratto'])) {
             echo '.tr('ABBONATO').'};
 echo '
