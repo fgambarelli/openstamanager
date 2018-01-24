ca7024678d6efdfda666b73678e8b349a2ba514b
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-01-24 23:00:05 +0100

    STAMPE

--- include/top.php
+++ include/top.php
@@ -13,7 +13,7 @@ echo '
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>'.$pageTitle.' - '.tr('OpenSTAManager').'</title>
+        <title>'.$pageTitle.'</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
