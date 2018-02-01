e4b89f3d33755703f7de5128c1a40b00029259f3
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-01 11:40:37 +0100

    FATTURE - Stampe

--- templates/fatture/header.php
+++ templates/fatture/header.php
@@ -3,15 +3,15 @@
 echo '
 <!-- Intestazione fornitore -->
 <div class="row">
-    <div class="col-xs-6">
+    <div class="col-xs-5">
         <img src="'.__DIR__.'/logo_azienda.jpg" alt="Logo" border="0"/>
     </div>
-    <div class="col-xs-6 text-right">
+    <div class="col-xs-7 text-right">
         <p><b>$f_ragionesociale$</b></p>
-        <p>'.tr('Sede : ').'$f_indirizzo$ $f_citta_full$</p>
-        <p>'.(!empty($f_telefono) ? tr('Tel/Fax').': ' : '').'$f_telefono$'.tr(' Cell.: ').' $f_cellulare$</p>
+        <p>'.tr('Sede ').'$f_indirizzo$ $f_citta_full$</p>
+        <p>'.(!empty($f_telefono) ? tr('Tel/Fax').': ' : '').'$f_telefono$'.tr(' Cell. ').' $f_cellulare$</p>
         <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$</p>
-		<p class="small">'.(!empty($f_fax) ? tr('Email ').': ' : '').'$f_email$'.(!empty($f_sitoweb) ? tr(' PEC ').': ' : '').'$f_sitoweb$</p>
+		<p>'.(!empty($f_fax) ? tr('Email ').': ' : '').'$f_email$'.(!empty($f_sitoweb) ? tr(' PEC ').': ' : '').'$f_sitoweb$</p>
         <p>'.(!empty($f_codiceri) ? tr('Registro Imprese.').': ' : '').'$f_codiceri$</p>
     </div>
 </div>
