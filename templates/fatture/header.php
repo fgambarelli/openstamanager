6c5b077c2a077bfa852bdae31cb07da06a343f55
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-16 09:16:04 +0100

    IVA - Arrotondamento

--- templates/fatture/header.php
+++ templates/fatture/header.php
@@ -3,16 +3,16 @@
 echo '
 <!-- Intestazione fornitore -->
 <div class="row">
-    <div class="col-xs-5">
+    <div class="col-xs-4">
         <img src="'.__DIR__.'/logo_azienda.jpg" alt="Logo" border="0"/>
     </div>
-    <div class="col-xs-7 text-right">
+    <div class="col-xs-8 text-right">
         <p><b>$f_ragionesociale$</b></p>
         <p>'.tr('Sede ').'$f_indirizzo$ $f_citta_full$</p>
         <p>'.(!empty($f_telefono) ? tr('Tel/Fax').': ' : '').'$f_telefono$'.tr(' Cell. ').' $f_cellulare$</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$</p>
+        <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$'.tr(' Reg.Imp. ').' $f_codiceri$</p>
 		<p>'.(!empty($f_fax) ? tr('Email ').': ' : '').'$f_email$'.(!empty($f_sitoweb) ? tr(' PEC ').': ' : '').'$f_sitoweb$</p>
-        <p>'.(!empty($f_codiceri) ? tr('Reg.Imprese.').': ' : '').'$f_codiceri$'.tr(' IBAN ').' $f_codiceiban$</p>
+        <p>'.(!empty($f_codiceiban) ? tr('Banca di appoggio ').': ' : '').'$f_appoggiobancario$'.tr(' IBAN ').' $f_codiceiban$</p>
     </div>
 </div>
 
