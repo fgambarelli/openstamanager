c73546488b278fc123283f9d2fa1a3dda982e105
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-16 12:15:14 +0100

    STAMPE

--- templates/base/header.php
+++ templates/base/header.php
@@ -11,15 +11,15 @@
 
 return '
 <div class="row">
-    <div class="col-xs-6">
-        <img src="'.DOCROOT.'/templates/'.$ptype.'/logo_azienda.jpg" alt="Logo" border="0"/>
+    <div class="col-xs-4">
+        <img src="'.__DIR__.'/logo_azienda.jpg" alt="Logo" border="0"/>
     </div>
-    <div class="col-xs-6 text-right">
-        <p><b>'.$f_ragionesociale.'</b></p>
-        <p>'.$f_indirizzo.' '.$f_citta_full.'</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva').': '.$f_piva : '').'</p>
-        <p>'.(!empty($f_codicefiscale) ? tr('C.F.').': '.$f_codicefiscale : '').'</p>
-        <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': '.$f_capsoc : '').'</p>
-        <p>'.(!empty($f_telefono) ? tr('Tel').': '.$f_telefono : '').'</p>
+    <div class="col-xs-8 text-right">
+        <p><b>$f_ragionesociale$</b></p>
+        <p>'.tr('Sede ').'$f_indirizzo$ $f_citta_full$</p>
+        <p>'.(!empty($f_telefono) ? tr('Tel/Fax').': ' : '').'$f_telefono$'.tr(' Cell. ').' $f_cellulare$</p>
+		<p>'.(!empty($f_fax) ? tr('Email ').': ' : '').'$f_email$'.(!empty($f_sitoweb) ? tr(' PEC ').': ' : '').'$f_sitoweb$</p>		
+        <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$'.tr(' Reg.Imp. ').' $f_codiceri$</p>
+        <p>'.(!empty($f_codiceiban) ? tr('Banca di appoggio ').': ' : '').'$f_appoggiobancario$'.tr(' IBAN ').' $f_codiceiban$</p>
     </div>
 </div>';
