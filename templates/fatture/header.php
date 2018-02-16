944873a7c072def4f0a96ac24327a1b62283472b
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-16 11:00:08 +0100

    IBAN

--- templates/fatture/header.php
+++ templates/fatture/header.php
@@ -9,8 +9,8 @@ echo '
         <p><b>$f_ragionesociale$</b></p>
         <p>'.tr('Sede ').'$f_indirizzo$ $f_citta_full$</p>
         <p>'.(!empty($f_telefono) ? tr('Tel/Fax').': ' : '').'$f_telefono$'.tr(' Cell. ').' $f_cellulare$</p>
+        <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$'.tr(' Reg.Imp. ').' $f_codiceri$</p>
 		<p>'.(!empty($f_fax) ? tr('Email ').': ' : '').'$f_email$'.(!empty($f_sitoweb) ? tr(' PEC ').': ' : '').'$f_sitoweb$</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva ').': ' : '').'$f_piva$'.(!empty($f_codicefiscale) ? tr(' C.F. ').': ' : '').'$f_codicefiscale$'.tr(' Reg.Imp. ').' $f_codiceri$</p>		
         <p>'.(!empty($f_codiceiban) ? tr('Banca di appoggio ').': ' : '').'$f_appoggiobancario$'.tr(' IBAN ').' $f_codiceiban$</p>
     </div>
 </div>
@@ -59,57 +59,6 @@ echo '
                     <p class="small-bold">'.tr('Banca di appoggio', [], ['upper' => true]).'</p>
                     <p>$c_appoggiobancario$</p>
                 </td>
-            </tr>
-        </table>
-    </div>
-    <div class="col-xs-5 col-xs-offset-1">
-        <table class="table" style="width:100%;margin-top:5mm;">
-            <tr>
-                <td colspan=2 class="border-full" style="height:20mm;">
-                    <p class="small-bold">'.tr('Spett.le', [], ['upper' => true]).'</p>
-                    <p><b>$c_ragionesociale$</b></p>
-                    <p>$c_indirizzo$ $c_citta_full$</p>
-                </td>
-            </tr>
-            <tr>
-                <td class="border-bottom border-left">
-                    <p class="small-bold">'.tr('Partita IVA', [], ['upper' => true]).'</p>
-                </td>
-                <td class="border-right border-bottom text-right">
-                    <p>$c_piva$</p>
-                </td>
-            </tr>
-            <tr>
-                <td class="border-bottom border-left">
-                    <p class="small-bold">'.tr('Codice fiscale', [], ['upper' => true]).'</p>
-                </td>
-                <td class="border-right border-bottom text-right">
-                    <p>$c_codicefiscale$</p>
-                </td>
-            </tr>
-        </table>
-    </div>
-</div>
-<div class="row">
-    <!-- Dati Pagamento -->
-    <div class="col-xs-7">
-        <table class="table">
-            <tr>
-                <td colspan="2" style="height:10mm;padding-top:2mm;">
-                    <p class="small-bold">'.tr('Pagamento', [], ['upper' => true]).'</p>
-                    <p>$pagamento$</p>
-                </td>
-				<td colspan="3" style="height:10mm;padding-top:2mm;">
-                    <p class="small-bold">'.tr('Banca di appoggio', [], ['upper' => true]).'</p>
-                    <p>$c_appoggiobancario$</p>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3" style="height:10mm;padding-top:2mm;">
-                    <p class="small-bold">'.tr('IBAN').'</p>
-                    <p>$c_codiceiban$</p>
-                </td>
-
             </tr>
         </table>
     </div>
