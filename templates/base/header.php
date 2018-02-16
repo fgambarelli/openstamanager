031f538a7e4d3d1ffa7236d95fdce8c419f889b2
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-16 12:24:53 +0100

    STAMPE

--- templates/base/header.php
+++ templates/base/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Header di default.
  * I contenuti di questo file vengono utilizzati per generare l'header delle stampe nel caso non esista un file header.php all'interno della stampa.
@@ -7,17 +8,17 @@
  *
  * La personalizzazione specifica dell'header deve comunque seguire lo standard della cartella custom: anche se il file header.php non esiste nella stampa originaria, se si vuole personalizzare l'header bisogna crearlo all'interno della cartella custom.
  */
+
 return '
 <div class="row">
-    <div class="col-xs-6">
+    <div class="col-xs-5">
         <img src="'.DOCROOT.'/templates/'.$ptype.'/logo_azienda.jpg" alt="Logo" border="0"/>
     </div>
-    <div class="col-xs-6 text-right">
+    <div class="col-xs-7 text-right">
         <p><b>'.$f_ragionesociale.'</b></p>
         <p>'.$f_indirizzo.' '.$f_citta_full.'</p>
-        <p>'.(!empty($f_piva) ? tr('P.Iva').': '.$f_piva : '').'</p>
-        <p>'.(!empty($f_codicefiscale) ? tr('C.F.').': '.$f_codicefiscale : '').'</p>
-        <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': '.$f_capsoc : '').'</p>
+        <p>'.(!empty($f_piva) ? tr('P.Iva').': '.$f_piva : '').'
+        '.(!empty($f_codicefiscale) ? tr('C.F.').': '.$f_codicefiscale : '').'</p>
         <p>'.(!empty($f_telefono) ? tr('Tel').': '.$f_telefono : '').'</p>
     </div>
 </div>';
