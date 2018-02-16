c7f07d7e97fee3a248f43da26714228764160a65
Francesca Gambarelli <34273200+fgambarelli@users.noreply.github.com>
2018-02-16 10:51:29 +0100

    IBAN

--- templates/fatture/header.php
+++ templates/fatture/header.php
@@ -1,5 +1,4 @@
 <?php
-
 echo '
 <!-- Intestazione fornitore -->
 <div class="row">
@@ -15,64 +14,92 @@ echo '
         <p>'.(!empty($f_codiceiban) ? tr('Banca di appoggio ').': ' : '').'$f_appoggiobancario$'.tr(' IBAN ').' $f_codiceiban$</p>
     </div>
 </div>
-
 <br>
-
 <div class="row">
     <!-- Dati Fattura -->
     <div class="col-xs-6">
         <div class="text-left" style="height:5mm;">
             <b>$tipo_doc$</b>
         </div>
-
         <table class="table">
             <tr>
                 <td valign="top" class="border-full text-center">
                     <p class="small-bold">'.tr('Nr. documento', [], ['upper' => true]).'</p>
-
                 </td>
-
                 <td class="border-right border-bottom border-top text-center">
                     <p class="small-bold">'.tr('Data documento', [], ['upper' => true]).'</p>
-
                 </td>
-
-
                 <td class="border-right border-bottom border-top center text-center">
                     <p class="small-bold">'.tr('Foglio', [], ['upper' => true]).'</p>
-
                 </td>
             </tr>
             <tr>
                 <td valign="top" class="border-full text-center">
-
                     <p><b>$numero_doc$</b></p>
                 </td>
-
                 <td class="border-right border-bottom border-top text-center">
-
                     <p><b>$data$</b></p>
                 </td>
-
                 <td class="border-right border-bottom border-top center text-center">
                     <p>{PAGENO}/{nb}</p>
                 </td>
             </tr>
-       </table>
+            <tr>
+                <td colspan="2" style="height:10mm;padding-top:2mm;">
+                    <p class="small-bold">'.tr('Pagamento', [], ['upper' => true]).'</p>
+                    <p>$pagamento$</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="height:10mm;padding-top:2mm;">
+                    <p class="small-bold">'.tr('IBAN').'</p>
+                    <p>$c_codiceiban$</p>
+                </td>
+                <td colspan="2" style="height:10mm;padding-top:2mm;">
+                    <p class="small-bold">'.tr('Banca di appoggio', [], ['upper' => true]).'</p>
+                    <p>$c_appoggiobancario$</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="col-xs-5 col-xs-offset-1">
+        <table class="table" style="width:100%;margin-top:5mm;">
+            <tr>
+                <td colspan=2 class="border-full" style="height:20mm;">
+                    <p class="small-bold">'.tr('Spett.le', [], ['upper' => true]).'</p>
+                    <p><b>$c_ragionesociale$</b></p>
+                    <p>$c_indirizzo$ $c_citta_full$</p>
+                </td>
+            </tr>
+            <tr>
+                <td class="border-bottom border-left">
+                    <p class="small-bold">'.tr('Partita IVA', [], ['upper' => true]).'</p>
+                </td>
+                <td class="border-right border-bottom text-right">
+                    <p>$c_piva$</p>
+                </td>
+            </tr>
+            <tr>
+                <td class="border-bottom border-left">
+                    <p class="small-bold">'.tr('Codice fiscale', [], ['upper' => true]).'</p>
+                </td>
+                <td class="border-right border-bottom text-right">
+                    <p>$c_codicefiscale$</p>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
-	
 <div class="row">
-    <!-- Dati PAGAMENTO -->
+    <!-- Dati Pagamento -->
     <div class="col-xs-7">
-
-	   <table class="table">
+        <table class="table">
             <tr>
-                <td colspan="1" style="height:10mm;padding-top:2mm;">
+                <td colspan="2" style="height:10mm;padding-top:2mm;">
                     <p class="small-bold">'.tr('Pagamento', [], ['upper' => true]).'</p>
-                    <p>$pagamento$</p>0
+                    <p>$pagamento$</p>
                 </td>
-                <td colspan="3" style="height:10mm;padding-top:2mm;">
+				<td colspan="3" style="height:10mm;padding-top:2mm;">
                     <p class="small-bold">'.tr('Banca di appoggio', [], ['upper' => true]).'</p>
                     <p>$c_appoggiobancario$</p>
                 </td>
@@ -82,8 +109,35 @@ echo '
                     <p class="small-bold">'.tr('IBAN').'</p>
                     <p>$c_codiceiban$</p>
                 </td>
-            </tr>
 
+            </tr>
+        </table>
+    </div>
+    <div class="col-xs-5 col-xs-offset-1">
+        <table class="table" style="width:100%;margin-top:5mm;">
+            <tr>
+                <td colspan=2 class="border-full" style="height:20mm;">
+                    <p class="small-bold">'.tr('Spett.le', [], ['upper' => true]).'</p>
+                    <p><b>$c_ragionesociale$</b></p>
+                    <p>$c_indirizzo$ $c_citta_full$</p>
+                </td>
+            </tr>
+            <tr>
+                <td class="border-bottom border-left">
+                    <p class="small-bold">'.tr('Partita IVA', [], ['upper' => true]).'</p>
+                </td>
+                <td class="border-right border-bottom text-right">
+                    <p>$c_piva$</p>
+                </td>
+            </tr>
+            <tr>
+                <td class="border-bottom border-left">
+                    <p class="small-bold">'.tr('Codice fiscale', [], ['upper' => true]).'</p>
+                </td>
+                <td class="border-right border-bottom text-right">
+                    <p>$c_codicefiscale$</p>
+                </td>
+            </tr>
         </table>
     </div>
 </div>';
