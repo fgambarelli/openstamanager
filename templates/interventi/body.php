3104217b04013083a6c0cb89406a44f248302ed2
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-02-21 14:17:39 +0100

    STAMPE - Nome File

--- templates/interventi/body.php
+++ templates/interventi/body.php
@@ -1,7 +1,7 @@
 <?php
 include_once __DIR__.'/../../core.php';
 include_once $docroot.'/modules/interventi/modutil.php';
-$report_name = 'intervento_'.$idintervento.'.pdf';
+$report_name = 'intervento_'.$intervento_numero.'.pdf';
 /*
     Dati intervento
 */
