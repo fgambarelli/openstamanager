94f1bd83caba3e3f3f1b95524031d8cd0df7b41c
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-02-21 14:03:03 +0100

    STAMPA - nome file

--- templates/fatture/body.php
+++ templates/fatture/body.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__.'/../../core.php';
 
-$report_name = 'fattura_'.$numero.'.pdf';
+$report_name = 'fattura_'.$numero.'_del_'.$data.'-'$c_ragionesociale'.pdf';
 
 $autofill = [
     'count' => 0, // Conteggio delle righe
