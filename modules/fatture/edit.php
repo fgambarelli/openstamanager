af582e8b65728676919aa47740a7dfb5c1528fdf
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-02-21 18:56:16 +0100

    FATTURE - Email

--- modules/fatture/edit.php
+++ modules/fatture/edit.php
@@ -27,13 +27,13 @@ $_SESSION['superselect']['ddt'] = $dir;
 		</div>
 
 		<div class="panel-body">
-		
+
 			<?php
 				if ($dir == 'entrata') {
 					$rs2 = $dbo->fetchArray('SELECT piva, codice_fiscale, citta, indirizzo, cap, provincia FROM an_anagrafiche WHERE idanagrafica='.prepare($records[0]['idanagrafica']));
 					$campi_mancanti = [];
-					
-					
+
+
 					if ($rs2[0]['piva'] == '') {
 						if ($rs2[0]['codice_fiscale'] == '') {
 							array_push($campi_mancanti, 'codice fiscale');
@@ -48,7 +48,7 @@ $_SESSION['superselect']['ddt'] = $dir;
 					if ($rs2[0]['cap'] == '') {
 						array_push($campi_mancanti, 'C.A.P.');
 					}
-					
+
 
 					if (sizeof($campi_mancanti) > 0) {
 						echo "<div class='alert alert-warning'><i class='fa fa-warning'></i> Prima di procedere alla stampa completa i seguenti campi dell'anagrafica:<br/><b>".implode(', ', $campi_mancanti).'</b><br/>
@@ -56,16 +56,16 @@ $_SESSION['superselect']['ddt'] = $dir;
 					}
 				}
 			?>
-			
-				
+
+
             <div class="pull-right">
-			
+
                 <button type="button" class="btn btn-primary" onclick="if( confirm('Duplicare questa fattura?') ){ $('#form-copy').submit(); }"><i class="fa fa-copy"></i> <?php echo tr('Duplica fattura'); ?></button>
 
 				<button type="submit" class="btn btn-success"><i class="fa fa-check"></i> <?php echo tr('Salva modifiche'); ?></button>
-				
+
 			</div>
-			
+
 			<div class="clearfix"></div>
 
 			<div class="row">
@@ -360,6 +360,18 @@ if ($records[0]['stato'] != 'Pagato' && $records[0]['stato'] != 'Emessa') {
 <?php
 //stampa solo per fatture di vendita
 if ($dir == 'entrata') {
+
+
+    if (( $records[0]['stato'] != 'Bozza' ) AND ( $records[0]['stato'] != 'Annullata' )) {
+        ?>
+        <button type="button" class="btn btn-info btn-sm" onclick="launch_modal( 'Invia fattura tramite email', '<?php echo $rootdir ?>/modules/fatture/form_email.php?id_module=<?php echo $id_module ?>&iddocumento=<?php echo $id_record ?>',1);"><i class="fa fa-envelope"></i> Invia email</button>&nbsp;
+        <?php
+        }else{
+        ?>
+        <button type="button" class="btn btn-info btn-sm" onclick="alert('La fattura deve essere prima Emessa per essere inviata!')"><i class="fa fa-envelope"></i> Invia email</button>&nbsp;
+        <?php
+        }
+
 	if (sizeof($campi_mancanti) > 0) {
 		/*echo "<div class='alert alert-warning'><i class='fa fa-warning'></i> Prima di procedere alla stampa completa i seguenti campi dell'anagrafica:<br/><b>".implode(', ', $campi_mancanti).'</b><br/>
 		'.Modules::link('Anagrafiche', $records[0]['idanagrafica'], tr('Vai alla scheda anagrafica'), null).'</div>';*/
