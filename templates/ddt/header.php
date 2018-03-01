91e76c0566e7163e32f32257d961f4508e0b9c46
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-03-01 23:10:16 +0100

    Pagamento DDT

--- templates/ddt/header.php
+++ templates/ddt/header.php
@@ -49,7 +49,7 @@ $default_header$
             <tr>
                 <td colspan="4" class="border-full" style="height:11mm;">
                     <p class="small-bold">'.tr('Pagamento', [], ['upper' => true]).'</p>
-                    <p>$c_pagamento$</p>
+                    <p>$pagamento$</p>
                 </td>
             </tr>
         </table>
