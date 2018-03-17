f16f71812f073662fb61f53e258ab156d0cb4833
Chica <chica@MacBook-Pro-di-Francesca.local>
2018-03-17 11:28:40 +0100

    PREVENTIVI STAMPE

--- templates/preventivi/body.php
+++ templates/preventivi/body.php
@@ -7,7 +7,7 @@ $report_name = 'preventivo_'.$idpreventivo.'.pdf';
 $autofill = [
     'count' => 0, // Conteggio delle righe
     'words' => 70, // Numero di parolo dopo cui contare una riga nuova
-    'rows' => 20, // Numero di righe massimo presente nella pagina
+    'rows' => 10, // Numero di righe massimo presente nella pagina
     'additional' => 10, // Numero di righe massimo da aggiungere
     'columns' => 5, // Numero di colonne della tabella
 ];
